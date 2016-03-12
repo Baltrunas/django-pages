4fbab96464db025bfe6d52a6649010899375f28c
Spell28 <BacuJlek@gmail.com>
2016-03-12 17:08:06 +0600

    Update slider

--- templatetags/media.py
+++ templatetags/media.py
@@ -5,9 +5,9 @@ from ..models import Media
 
 
 @register.simple_tag(takes_context=True)
-def media_group(context, group, tpl='pages/__blocks/media.html'):
+def media_group(context, group, tpl='pages/__blocks/media.html', size=''):
 	page = context['page']
-
+	context['size'] = size
 	context['media_group'] = Media.objects.filter(page=page, group=group, public=True)
 
 	tpl = template.loader.get_template(tpl)
